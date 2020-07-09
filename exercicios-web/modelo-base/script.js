24760a225a78034f729df0dda9ff07a7cb6e9046
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2020-07-09 11:37:09 -0300

    Modelo base para exercicios javascript-web

---
+++ exercicios-web/modelo-base/script.js
@@ -0,0 +1,21 @@
+/*
+    A finalidade deste programa é aplicar conceitos do curso de JavaScript.
+    Este programa será o modelo-base para outros exercícios.
+
+    Copyright (C) 2020 ProgrammerHero.com.br
+
+    Este programa é um software gratuito: você pode redistribuí-lo e / ou modificar
+    sob os termos da Licença Pública Geral GNU, publicada pela
+    Free Software Foundation, versão 3 da Licença ou
+    (a seu critério) qualquer versão posterior.
+
+    Este programa é distribuído na esperança de que seja útil,
+    mas SEM QUALQUER GARANTIA; sem sequer a garantia implícita de
+    COMERCIALIZAÇÃO ou ADEQUAÇÃO PARA UMA FINALIDADE ESPECÍFICA. Veja o
+    GNU General Public License para mais detalhes.
+
+    Você deveria ter recebido uma cópia da Licença Pública Geral GNU
+    junto com este programa. Caso contrário, consulte <http://www.gnu.org/licenses/>.
+*/
+
+alert('Olá')

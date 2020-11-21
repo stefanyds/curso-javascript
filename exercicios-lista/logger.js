6ead8d237dc1df7489a69f9cefff45acb4d0daff
stefanyds <ste.stefany22@gmail.com>
2020-11-21 11:14:50 -0300

    Chore(logger): adding logs to file logs/exercicios.log

--- exercicios-lista/logger.js
+++ exercicios-lista/logger.js
@@ -1,7 +1,20 @@
-const { createLogger, transports } = require('winston');
+const { createLogger, transports, format } = require('winston');
 
 module.exports = createLogger({
+  format: format.combine(
+    format.simple(),
+    format.timestamp(),
+    format.printf(
+      (info) =>
+        `[${info.timestamp}] ${info.level.toUpperCase()} ${info.message}`
+    )
+  ),
   transports: [
+    new transports.File({
+      maxsize: 5120000,
+      maxFiles: 5,
+      filename: `${__dirname}/logs/exercicios.log`,
+    }),
     new transports.Console({
       level: 'debug',
     }),

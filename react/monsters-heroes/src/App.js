63df66c569d03622b7eb9b521868da8f7675e73d
stefanyds <ste.stefany22@gmail.com>
2021-02-20 12:23:23 -0300

    feat: render monsters' name from remote json

--- react/monsters-heroes/src/App.js
+++ react/monsters-heroes/src/App.js
@@ -6,22 +6,16 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstain',
-          id: '1',
-        },
-        {
-          name: 'Drácula',
-          id: '2',
-        },
-        {
-          name: 'Zombie',
-          id: '3',
-        },
-      ],
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
